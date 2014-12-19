b0e68fbfd7015fd51da2eba2cd89c665d900071c
Dan Underwood <underwd2@cs.man.ac.uk>
2014-12-19 17:33:22 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# ARM assembly highlighting for Atom
+
+Adds syntax highlighting for the ARM Assembly language to Atom.
+
+## Install
+
+To install the package language-arm:
+
+* In Atom: (Preferences->Packages)
+* Shell: `$ apm install language-arm`
+
+## TODO
+
+This is a quickly knocked together version of the syntax highlighting package found in Package Control for Sublime Text but has been rewritten in places to make it a bit cleaner! There are still some things missing and it could do with an entire refactor! Feel free to report issues or submit pull requests.
---
+++ test.s
@@ -0,0 +1,31 @@
+;--------------------------------------------------------------------
+;	ARM Examples for testing of syntax highlighting
+;	Dan Underwood
+;--------------------------------------------------------------------
+
+strings 	defw	"Awaiting input"
+		defb    'a', 'b', 'c', 'd'
+
+branches	b	start                ;With comment!
+                bne     end
+                bl      linked_place
+
+                include test.s
+
+stack           defs	512			;stack
+stack_end					;end stack
+
+start	        adr	sp, stack_end
+                mrs	r0, r1
+                bic	r2, r0, #&3F
+                mov	r1, sp
+                strb	r0, [r1, #stack]
+                nop
+                stmfd	sp!, {r0-r2,lr}
+		movs	r2, r2, lsr #1
+		cmp	r0, r2
+                addsne	r0, r0, #&1
+                bne	start
+		ldmfd	sp!, {r0-r2,pc}^
+
+end		svc	0
